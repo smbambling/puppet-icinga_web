95d39e8b7f5ca93b06b236d037a27f2da56b2606
Steven Bambling <smbambling@gmail.com>
2014-09-25 21:22:17 -0700

    cleaning up the logic and preping for mysql compatibility

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,14 +10,17 @@
 # $manage_repos = true/false
 #
 # Which IDO backend should be used, I prefer PostgreSQL so its default :)
-# $ido_backend = pgsql/mysql
+# This should be updated to support mysql if desired
+# $server_db_type = pgsql
 
 class icinga_web (
   $manage_repos = true,
-  $db_backend = 'pqsql',
+  $server_db_type = 'pqsql',
   $db_user = 'icingaweb',
-  $db_pass = 'icingaweb',
+  $db_password = 'icingaweb',
   $db_name = 'icingaweb',
+  $db_host = 'localhost',
+  $db_port = '5432'
   ) {
 
   if $manage_repos == true {
@@ -26,7 +29,6 @@ class icinga_web (
       'CentOS': {
 
         #Add the official Icinga Yum repository: http://packages.icinga.org/epel/
-
         #Check to see if the Yumrepo class is already defined from the icinga module
         if !defined ( Yumrepo['icinga2_yum_repo'] ) {
 
@@ -56,7 +58,7 @@ class icinga_web (
   $dependencies_pkgs = [ 'php', 'php-cli', 'php-pear', 'php-xmlrpc', 'php-pdo', 'php-soap', 'php-gd', 'php-ldap', 'php-pgsql' ]
 
   # Icinga Web Packages
-  $icinga_web_pkgs = [ 'icinga-web', "icinga-web-${db_backend}", 'icinga-web-scheduler' ]
+  $icinga_web_pkgs = [ 'icinga-web', "icinga-web-${server_db_type}", 'icinga-web-scheduler' ]
 
   # Install Packages
   package { $dependencies_pkgs:
@@ -76,7 +78,7 @@ class icinga_web (
     mode    => '0644',
     content => template('icinga_web/databases.xml.erb'),
     notify  => Exec['icinga clear cache'],
-    require => [ Package['icinga-web'], Package["icinga-web-${db_backend}"] ],
+    require => [ Package['icinga-web'], Package["icinga-web-${server_db_type}"] ],
   }
 
   # Clear Icinga Web cache after updating the xml configuration file
@@ -85,8 +87,24 @@ class icinga_web (
     refreshonly => true,
   }
 
+  #Pick set the right path where we can find the DB schema based on the OS...
+  case $::operatingsystem {
+    'CentOS': {
+      #...and database that the user picks
+      case $server_db_type {
+        'mysql': { $server_db_schema_path = '/usr/share/icinga-web/etc/schema/mysql.sql' }
+        'pgsql': { $server_db_schema_path = '/usr/share/icinga-web/etc/schema/pgsql.sql' }
+        default: { fail("${server_db_type} is not supported") }
+      }
+    }
+
+  #Fail if we're on any other OS:
+    default: { fail("${::operatingsystem} is not supported!") }
+  }
+
+  # Load the Icinga Web schema if not already loaded
   exec { 'icinga_web load schema':
-    command => "/usr/bin/sudo /sbin/runuser -l postgres -c 'psql -U ${db_user} -d ${db_name} < /usr/share/icinga-web/etc/schema/pgsql.sql'",
+    command => "/usr/bin/sudo /sbin/runuser -l postgres -c 'psql -U ${db_user} -d ${db_name} < ${server_db_schema_path}'",
     require => Package['icinga-web-pgsql'],
     onlyif  => "/usr/bin/sudo /sbin/runuser -l postgres -c \"/usr/bin/psql -U ${db_user} -d ${db_name} -t -c \"SELECT version FROM nsm_db_version\"\" 2>&1 >/dev/null | grep -q \"does not exist\"",
   }
